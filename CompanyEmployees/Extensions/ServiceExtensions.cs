f08fdf42be1976c3af73e1cf3e9210f9f14ea834
LizaKamen <lizakamenoff@gmail.com>
2022-12-19 23:54:46 +0300

    Set up the configuration

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -1,7 +1,10 @@
-﻿namespace CompanyEmployees.Extensions
+﻿using Contracts;
+
+namespace CompanyEmployees.Extensions
 {
     public static class ServiceExtensions
     {
+        public static void ConfigureLoggerService(this IServiceCollection services) => services.AddSingleton<ILoggerManager, ILoggerManager>();
         public static void ConfigureCors(this IServiceCollection services) =>
             services.AddCors(options =>
             {
