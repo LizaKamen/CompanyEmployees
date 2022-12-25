fddb3bb894723fc4510e8718b54ed8b3467e29c8
LizaKamen <lizakamenoff@gmail.com>
2022-12-25 17:59:59 +0300

    Create RepositoryManager

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -1,11 +1,13 @@
 ﻿using Contracts;
 using LoggerService;
+using Repository;
 
 namespace CompanyEmployees.Extensions
 {
     public static class ServiceExtensions
     {
         public static void ConfigureLoggerService(this IServiceCollection services) => services.AddSingleton<ILoggerManager, LoggerManager>();
+        public static void ConfigureRepositoryManager(this IServiceCollection services) => services.AddScoped<IRepositoryManager, RepositoryManager>();
         public static void ConfigureCors(this IServiceCollection services) =>
             services.AddCors(options =>
             {
