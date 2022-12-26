d846882020281fdd6e40409d94b86744eb327e9c
LizaKamen <lizakamenoff@gmail.com>
2022-12-27 00:02:26 +0300

    Add Service Layer and some fixes

---
+++ Service/ServiceManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contracts;
+using Service.Contracts;
+
+namespace Service
+{
+    public sealed class ServiceManager : IServiceManager
+    {
+        private readonly Lazy<ICompanyService> _companyService;
+        private readonly Lazy<IEmployeeService> _employeeService;
+
+        public ServiceManager(IRepositoryManager repository, ILoggerManager logger)
+        {
+            _companyService = new Lazy<ICompanyService>(() => new CompanyService(repository, logger));
+            _employeeService = new Lazy<IEmployeeService>(() => new EmployeeService(repository, logger));
+        }
+
+        public IEmployeeService EmployeeService => _employeeService.Value;
+        public ICompanyService CompanyService => _companyService.Value;
+    }
+}
