f43feaf628efae751fe4278ebdb4a1c653ce941e
LizaKamen <lizakamenoff@gmail.com>
2023-01-20 19:02:26 +0300

    Add HATEOAS
    Not working

--- Service/DataShaping/DataShaper.cs
+++ Service/DataShaping/DataShaper.cs
@@ -1,4 +1,5 @@
 ﻿using Contracts;
+using Entities.Models;
 using System.Dynamic;
 using System.Reflection;
 
@@ -12,7 +13,7 @@ namespace Service.DataShaping
             Properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
         }
 
-        public IEnumerable<ExpandoObject> ShapeData(IEnumerable<T> entities, string fieldsString)
+        public IEnumerable<ShapedEntity> ShapeData(IEnumerable<T> entities, string fieldsString)
         {
             var requiredProperties = GetRequiredProperties(fieldsString);
 
@@ -20,7 +21,7 @@ namespace Service.DataShaping
         }
 
 
-        public ExpandoObject ShapeData(T entity, string fieldsString)
+        public ShapedEntity ShapeData(T entity, string fieldsString)
         {
             var requiredProperties = GetRequiredProperties(fieldsString);
 
@@ -52,9 +53,9 @@ namespace Service.DataShaping
             return requiredProperties;
         }
 
-        private IEnumerable<ExpandoObject> FetchData(IEnumerable<T> entities, IEnumerable<PropertyInfo> requiredProperties)
+        private IEnumerable<ShapedEntity> FetchData(IEnumerable<T> entities, IEnumerable<PropertyInfo> requiredProperties)
         {
-            var shapedData = new List<ExpandoObject>();
+            var shapedData = new List<ShapedEntity>();
 
             foreach (var entity in entities)
             {
@@ -64,15 +65,18 @@ namespace Service.DataShaping
 
             return shapedData;
         }
-        private ExpandoObject FetchDataForEntity(T entity, IEnumerable<PropertyInfo> requiredProperties)
+        private ShapedEntity FetchDataForEntity(T entity, IEnumerable<PropertyInfo> requiredProperties)
         {
-            var shapedObject = new ExpandoObject();
+            var shapedObject = new ShapedEntity();
 
             foreach (var property in requiredProperties)
             {
                 var objectPropertyValue = property.GetValue(entity);
-                shapedObject.TryAdd(property.Name, objectPropertyValue);
+                shapedObject.Entity.TryAdd(property.Name, objectPropertyValue);
             }
+            var objectProperty = entity.GetType().GetProperty("Id");
+            shapedObject.Id = (Guid)objectProperty.GetValue(entity);
+
             return shapedObject;
         }
     }
